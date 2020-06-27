f728ec8b04e33b9810017eb58c2cedeaba05ac51
Vladyslav <bespalov.v@brights.cloud>
2020-06-27 20:07:06 +0300

    Bug fix. Create logout endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -30,7 +30,7 @@ router.post('/users/login', async (req, res) => {
 })
 
 router.get('/users/me', auth, async (req, res) => {
-    res.sens(req.user)
+    res.send(req.user)
 })
 
 module.exports = router
