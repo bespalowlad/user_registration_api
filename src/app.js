d6aaad0c2b7ff2dd867f26f85eb46ba66092d196
Vladyslav <bespalov.v@brights.cloud>
2020-06-28 14:27:53 +0300

    port fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const userRouter = require('./routers/user')
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 require('./db/db')
 
